322ed5fa0dbb36370cae8e7e8556fd5774c11f6c
Matthew Vandevort <vandevortmatthew@gmail.com>
2021-06-28 21:31:36 -0400

    added questions and more javascript

---
+++ questions.js
@@ -0,0 +1,35 @@
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title:
+        "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+      answer: "console.log"
+    },
+    {
+      title: "Which of these is NOT a flex direction property?",
+      choices: ["row", "column", "wrap", "column-reverse"],
+      answer: "wrap"
+    },
+    {
+      title:
+        "HTML is an acronym of:",
+      choices: ["HyperText Markup Language", "HyperText Machine Language", "HyperToken Markup Language", "HyperToken Machine Language"],
+      answer: "HyperText Markup Language"
+    },
+    {
+      title: "CSS is an acronym of",
+      choices: ["Coded Style Sheets", "Cascading Style Sheets", "Coding Style Sheets ", "Cascaded Style Sheets"],
+      answer: "Cascading Style Sheets"
+    },
+    {
+      title:
+        "In HTML, an ordered list is created using which tag?",
+      choices: ["<UL>", "<NL>", "<OL>", "<AL>"],
+      answer: "<OL>"
+    }
+  ];
--- script.js
+++ script.js
@@ -1,19 +1,77 @@
-var timeEl = document.querySelector(".time"); 
 
-var highEl = document.querySelector('.highscore');
-highEl.textContent = 'View Highscore';
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
 
-var secondsLeft = 75;
-function setTime() {
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = 'Time Left: ' + secondsLeft;
 
-    if(secondsLeft === 0) {
-      clearInterval(timerInterval);
-    }
+var timerEl = document.getElementById('time');
+var questionsEl = document.getElementById('questions');
+var choicesEl = document.getElementById('choices');
+var startBtn = document.getElementById('start-button');
+var initialsEl = document.getElementById('initials');
+var submitBtn = document.getElementById('submit');
+var feedbackEl = document.getElementById('feedback');
 
+
+function quizStart() {
+  var homeScreen = document.getElementById('home');
+  homeScreen.setAttribute('class', 'start hide');
+
+  questionsEl.setAttribute('class', " ");
+  
+  timerId = setInterval(function () {
+    clockTick();
+    
   }, 1000);
+    timerEl.textContent = time;
+  
+  getQuestion();
 }
 
-setTime();
+function getQuestion() {
+  var currentQuestion = questions[currentQuestionIndex];
+
+  questionsEl.children[0].textContent = currentQuestion.title;
+
+  while (choicesEl.hasChildNodes()) {
+    choicesEl.removeChild(choicesEl.lastChild);
+
+  }
+
+  for (let i = 0; i < currentQuestion.choices.length; i++) {
+    var choiceButton = document.createElement('button');
+    choiceButton.textContent = currentQuestion.choices[i];
+
+
+    choicesEl.appendChild(choiceButton);
+    
+  }
+
+  choicesEl.children[0].addEventListener("click", function(event){
+    questionAnswer(choicesEl.children[0]);
+  });
+  choicesEl.children[1].addEventListener("click", function(event){
+    questionAnswer(choicesEl.children[1]);
+  });
+  choicesEl.children[2].addEventListener("click", function(event){
+    questionAnswer(choicesEl.children[2]);
+  });
+  choicesEl.children[3].addEventListener("click", function(event){
+    questionAnswer(choicesEl.children[3]);
+  });
+
+
+}
+
+function questionAnswer(answerChoice) {
+  if (answerChoice.textContent != questions[currentQuestionIndex].answer) {
+    time -= 10;
+
+    feedbackEl
+  }
+}
+
+
+
+startBtn.onclick = quizStart;
+console.log(quizStart);
